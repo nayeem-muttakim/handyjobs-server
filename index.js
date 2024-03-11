8b36b0ad906a9f2d7a7d0ba0f30ca94ecd589882
Nohan550 <nohanmuttakim.bd@gmail.com>
2024-03-11 16:27:17 +0600

    jobs collection added

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const { MongoClient, ServerApiVersion } = require("mongodb");
 require("dotenv").config();
+const jwt = require("jsonwebtoken");
 const app = express();
 app.use(express.json());
 app.use(cors());
@@ -21,6 +22,43 @@ async function run() {
   try {
     // connect client to server
     // await client.connect();
+
+    const database = client.db("jobsDB");
+    const jobs = database.collection("jobs");
+
+    //  jwt
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      console.log(user);
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "2h",
+      });
+      res.send({ token });
+    });
+
+    // middlewares
+    const verifyToken = (req, res, next) => {
+      if (!req.headers.authorization) {
+        return res.status(401).send({ message: "unauthorized access" });
+      }
+      const token = req.headers.authorization.split(" ")[1];
+      // console.log(token);
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, dec) => {
+        if (err) {
+          return res.status(401).send({ message: "unauthorized access" });
+        }
+        req.dec = dec;
+
+        next();
+      });
+    };
+
+    app.post("/jobs", verifyToken, async (req, res) => {
+      const job = req.body;
+      const result = await jobs.insertOne(job);
+      res.send(result);
+    });
+
     //  send a ping for connection
     await client.db("admin").command({ ping: 1 });
     console.log("Connection successful");
